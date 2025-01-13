fd3e4b98d95547528e076ed0e4c806995e32380f
Azat Bagaviev <azat.bagaviev@haqq.network>
2025-01-13 10:46:23 +0300

    ci: add nix action

--- nix/tests/default.nix
+++ nix/tests/default.nix
@@ -1,12 +1,18 @@
 { inputs, ... }:
 {
   perSystem =
-    { pkgs, ... }:
+    {
+      config,
+      pkgs,
+      ...
+    }:
     {
       legacyPackages.nixosTests = {
         haqqd-basic = pkgs.callPackage ./basic.nix {
           imports = [ inputs.self.nixosModules.haqqd ];
         };
       };
+
+      checks = config.legacyPackages.nixosTests;
     };
 }
